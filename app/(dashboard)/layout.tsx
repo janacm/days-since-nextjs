2a8fbb68faefacb52203104786ac7374d32f953b
jan <janac.meena@gmail.com>
2025-06-10 16:43:32 -0400

    fix: resolve merge conflicts and update CI

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -38,12 +38,12 @@ export default function DashboardLayout({
     <Providers>
       <main className="flex min-h-screen w-full flex-col bg-muted/40">
         <DesktopNav />
+        <header className="sticky top-0 z-30 flex h-14 w-full items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+          <MobileNav />
+          <DashboardBreadcrumb />
+          <User />
+        </header>
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            <MobileNav />
-            <DashboardBreadcrumb />
-            <User />
-          </header>
           <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
             {children}
           </main>
